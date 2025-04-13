132d2bec9a2699f0d5f13259b50ab74fd1876a18
samsak1990 <samsak.developer@internet.ru>
2025-04-13 17:00:37 +0300

    Finish initial project

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,12 @@
+import { CssBaseline } from '@mui/material';
 import { createRoot } from 'react-dom/client';
 
 import App from './components/App.jsx';
 
-createRoot(document.getElementById('root')).render(<App />);
+createRoot(document.getElementById('root')).render(
+  <>
+    <CssBaseline>
+      <App />
+    </CssBaseline>
+  </>,
+);
